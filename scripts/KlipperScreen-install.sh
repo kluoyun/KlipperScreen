426f69062a9c48e5a8a562b1c3ae2d0e732d261c
kluoyun <31061246+kluoyun@users.noreply.github.com>
2021-12-28 16:30:48 +0800

    Update KlipperScreen-install.sh

--- scripts/KlipperScreen-install.sh
+++ scripts/KlipperScreen-install.sh
@@ -4,7 +4,7 @@ SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 KSPATH=$(sed 's/\/scripts//g' <<< $SCRIPTPATH)
 KSENV="${HOME}/.KlipperScreen-env"
 
-PKGLIST="xserver-xorg-video-fbturbo xdotool xinit xinput x11-xserver-utils libopenjp2-7 python3-distutils"
+PKGLIST="xserver-xorg-legacy xserver-xorg xserver-xorg-video-all xdotool xinit xinput x11-xserver-utils libopenjp2-7 python3-distutils"
 PKGLIST="${PKGLIST} python3-virtualenv virtualenv matchbox-keyboard wireless-tools"
 PKGLIST="${PKGLIST} libatlas-base-dev fonts-freefont-ttf"
 PKGLIST="${PKGLIST} libgirepository1.0-dev gcc libcairo2-dev pkg-config python3-dev gir1.2-gtk-3.0"
